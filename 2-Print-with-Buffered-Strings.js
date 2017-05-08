266be343ea4578b6b878f7dc1ac426cd14aed99a
farnaz <farnaz.rashidi@gmail.com>
2017-05-08 16:59:52 -0700

    Print the data using buffering technique

--- 2-Print-with-Buffered-Strings.js
+++ 2-Print-with-Buffered-Strings.js
@@ -1,20 +1,23 @@
+'use strict'
 const https = require('https');
 
-function getAndPrintHTMLChunks () {
+function getAndPrintHTML () {
   const requestOptions = {
     host: 'sytantris.github.io',
-    path: '/http-examples/step1.html'
+    path: '/http-examples/step2.html'
   };
 
   https.get(requestOptions, (res) => {
+    let buffer = '';
     res.setEncoding('utf-8');
     res.on('data', (data) => {
-      console.log(data.toString() +'\n');
+      buffer = buffer + data.toString();
     })
     res.on('end', () => {
+      console.log(buffer);
       console.log('response stream complete');
     });
   });
 }
 
-getAndPrintHTMLChunks();
+getAndPrintHTML();
